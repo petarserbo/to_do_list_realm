c4acb565cf488982cea610acd7739f3df4426263
serbo <petarserbo@gmail.com>
2021-04-27 17:41:21 +0300

    Initial Commit

---
+++ to_do_list_realm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  to_do_list_realm
+//
+//  Created by Petar Perich on 27.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
